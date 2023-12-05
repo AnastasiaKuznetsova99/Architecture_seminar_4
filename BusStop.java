075ef11950bc6cc9d53a9c5033bd9a4fcb960b64
Anastasia Kuznetsova <asiakisa11@gmail.com>
2023-12-05 22:49:40 +0500

    seminar 4

---
+++ BusStop.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+public class BusStop {
+    public int id;
+    public String name;
+    public double attitude;
+    public double lattitude;
+
+    public BusStop(int id, String name, double attitude, double lattitude) {
+        this.id = id;
+        this.name = name;
+        this.attitude = attitude;
+        this.lattitude = lattitude;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getAttitude() {
+        return attitude;
+    }
+
+    public double getLattitude() {
+        return lattitude;
+    }
+
+    public double[] getCoordinates() {
+        return new double[] { getAttitude(), getLattitude() };
+    }
+
+}
---
+++ TransportZone.java
@@ -0,0 +1,22 @@
+public class TransportZone {
+
+    private int id;
+    private String remark;
+
+    public TransportZone(int id, String remark) {
+        this.id = id;
+        this.remark = remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+}
